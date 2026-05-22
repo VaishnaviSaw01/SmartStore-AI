89cad76acefde4ebf1384ca4656ab8ac655cc835
VaishnaviSaw01 <gungunsaw924@gmail.com>
2026-05-22 13:31:21 +0530

    Added localStorage persistence for dynamic product management

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,26 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import MainLayout from "../layouts/MainLayout";
 import Navbar from "../components/Navbar";
 import ProductForm from "../components/ProductForm";
 import ProductTable from "../components/ProductTable";
-import type { Product } from "../types/product";
 
+import type { Product } from "../types/product";
 
 function Products() {
 
-  const [products, setProducts] = useState<Product[]>([
-    {
-      id: 1,
-      title: "Wireless Mouse",
-      price: 999,
-      category: "Electronics",
-      stock: 20,
-    },
-  ]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   const [editingProduct, setEditingProduct] =
     useState<Product | null>(null);
+
+  // LOAD PRODUCTS FROM LOCALSTORAGE
+  useEffect(() => {
+
+    const savedProducts =
+      localStorage.getItem("products");
+
+    if (savedProducts) {
+      setProducts(JSON.parse(savedProducts));
+    }
+
+  }, []);
+
+  // SAVE PRODUCTS TO LOCALSTORAGE
+  useEffect(() => {
+
+    localStorage.setItem(
+      "products",
+      JSON.stringify(products)
+    );
+
+  }, [products]);
 
   return (
     <MainLayout>
